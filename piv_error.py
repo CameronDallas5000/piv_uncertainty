8460d18be05f78617fef9c0385d28faa6daee703
CameronDallas5000 <cameron.alidal@gmail.com>
2017-02-21 10:32:30 -0500

    Changed the structure so the code uses the whole image, not interrogation windows. Still need final uncertainty output, Gaussian recursive filter and testing.

--- piv_error.py
+++ piv_error.py
@@ -11,8 +11,27 @@ Turbulence Research Laboratory
 
 import numpy as np
 
+def recursiveGaussSmooth(data, L):
 
-def correlation_stats(c, i_peak, j_peak, u_row, u_col, im1, im2):
+	"""
+	Fast imlimentation of a recursive gaussian smoother. 
+	For reference, see paper 'Tips & Tricks: Fast Image Filtering Algorithms (2007)'
+
+	Inputs:
+		data: 2d array
+			array that you want smoothed
+		L: int
+			filter length (effective interrogation window size)
+
+	Outputs:
+		filter: 2d array
+			smoothed array multiplied by square of the filter length
+	"""
+
+	
+
+
+def correlation_stats(frame_a, frame_b, x, y, u, v, dt, scaling_factor = None):
 
 	"""
 	Uses the Correlation Statistics method to approximate the error in the velocity
@@ -21,40 +40,83 @@ def correlation_stats(c, i_peak, j_peak, u_row, u_col, im1, im2):
 	by correlation statistics. 2014'
 
 	Inputs:
-		c = correlation map matrix
-		i_peak = row where c is max
-		j_peak = column where c is max
-		u_row = row image shift (from correlation map)
-		u_col = coloum image shift (from correlation map)
-		im1 = first frame IW
-		im2 = second frame IW
-
+		frame_a: 2d array
+			first full frame
+		frame_b: 2d array
+			second full frame 
+		x: 2d array 
+			x location of velocity vectors
+		y: 2d array
+			location of velocity vectors
+		u: 2d array 
+			x velocity
+		v: 2d array 
+			y velocity
+		dt: float  
+			time step between windows
+		scaling factor: int 
+			image scaling factor in pixels per meter. By default this assumes that all inputs for 
+			velocity and location are in units of pixels and pixels per second
+		  
 	Outputs:
-		Ux = x uncertainty in pixels
-		Uy = y uncertainty in pixels
+		Ux: 2d array
+			x velocity uncertainty in m/s
+		Uy: 2darray 
+			y velocity uncertainty in m/s
+
+	""" 
+
+
+	#ensure frames are the same shape
+	if frame_a.shape != frame_b.shape:
+		raise ValueError('Image pair must be the same shape') 
+
+	#get field shape
+	n_rows, n_cols = frame_a.shape
+
+	#Calculate Displacement field. 
+	#ie local shift of the interrogation window in pixels at each vector location
+	u_row = v*dt 	#row shift field
+	u_col = u*d 	#column shift field
+
+	#scale the displacement field if needed
+	if scaling_factor:
+		u_row = u_row*scaling_factor
+		v_row = v_row*scaling_factor
+
+	#to help with multiplying windows near boundary
+	rowShift_max = np.max(u_row) #probably a better way to do this
+	colShift_max = np.max(u_col)
 
 	"""
+	-----------------  x uncertainty -----------------------
+	"""
 
-	#get integer values for correlation max
-	i_peak = int(round(i_peak))
-	j_peak = int(round(j_peak))
+	#calculate C(u), C+, C-, and S_xy field for x
+	c_x = np.zeros(frame_a.shape)
+	cPlus_x = np.zeros(frame_a.shape)
+	cMinus_x = np.zeros(frame_a.shape)
+	S_x = np.zeros(frame_a.shape)
 
-	#dewarp images onto each other
+	for i in range(n_rows - rowShift_max):
+		for j in range(n_cols - colShift_max):
+			c_x[i,j] = frame_a[i,j]*frame_b[i-u_row[i,j], j-u_col[i,j]]
+			cPlus_x[i,j] = frame_a[i,j]*frame_b[i-u_row[i,j], j-u_col[i,j]+1]
+			cMinus_x[i,j] = frame_a[i,j]*frame_b[i-u_row[i,j], j-u_col[i,j]-1]
+
+			#calculate S
+			for k in range(5):
+				for l in range(5):
+					C_xy = frame_a[i, j]*frame_b[i-u_row[i,j], j-u_col[i,j]+1] - frame_a[i, j+1]*frame_b[i-u_row[i,j], j-u_col[i,j]]  
+					C_dxdy = frame_a[i+k, j+l]*frame_b[i+k-u_row[i,j], j+l-u_col[i,j]+1] - frame_a[i+k, j+l+1]*frame_b[i+k-u_row[i,j], j+l-u_col[i,j]]  
+					S_x[i,j] += C_xy*C_dxdy
 
 
 
-	#correlation map maximum
-	c0 = c[i_peak, j_peak]
+	"""
+	-----------------  y uncertainty -----------------------
+	"""
 
-	#x uncertiainty
-	c1_x = (c[i_peak, j_peak + 1] + c[i_peak, j_peak - 1])/2. - sig_x/2.
-	c2_x = (c[i_peak, j_peak + 1] + c[i_peak, j_peak - 1])/2. + sig_x/2.
-	Ux = 0.5*(np.log(c2_x) - np.log(c1_x))/(2*np.log(c0) - np.log(c2_x) - np.log(c1_x))
-
-	#y uncertiainty
-	c1_y = (c[i_peak + 1, j_peak] + c[i_peak - 1, j_peak])/2. - sig_y/2.
-	c2_y = (c[i_peak + 1, j_peak] + c[i_peak - 1, j_peak])/2. + sig_y/2.
-	Uy = 0.5*(np.log(c2_y) - np.log(c1_y))/(2*np.log(c0) - np.log(c2_x) - np.log(c1_x))
 
 	return Ux, Uy
 
